5a876cd3708c11caa2a4106cc6a08382603cd1ee
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-17 18:22:08 +0000

    test

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,77 +1,45 @@
 #!/usr/bin/python3
-""" parses logs """
 
+""" script that reads stdin line by line and computes metrics """
 
 import sys
 
 
-def main():
-    """ main func """
-    total_file_size = 0
-    order = [
-       "200",
-       "301",
-       "400",
-       "401",
-       "403",
-       "404",
-       "405",
-       "500"
-    ]
-    status_codes_count_map = {
-       "200": 0,
-       "301": 0,
-       "400": 0,
-       "401": 0,
-       "403": 0,
-       "404": 0,
-       "405": 0,
-       "500": 0
-    }
-
-    try:
-        count = 0
-        for line in sys.stdin:
-            tokens = line.split()
-            if len(tokens) == 9:
-                total_file_size += int(tokens[8])
-                count += 1
-                status_codes_count_map[tokens[7]] += 1
-
-                if count == 10:
-                    count = 0
-                    print_report(
-                        status_codes_count_map,
-                        total_file_size,
-                        order
-                    )
-
-    except KeyboardInterrupt:
-        print_report(
-            status_codes_count_map,
-            total_file_size,
-            order
-        )
-        raise
+def printsts(dic, size):
+    """ Prints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
 
-# def print_report(dct_, file_size, order):
-#     """ prints to stdout summary of logs """
-#     print("File size: {}".format(file_size))
-#     for key in order:
-#         if dct_.get(key):
-#             print("{}: {}".format(key, dct_[key]))
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
+
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
+
+        stlist = line.split()
+        count += 1
+
+        try:
+            size += int(stlist[-1])
+        except Exception:
+            pass
+
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except Exception:
+            pass
+    printsts(sts, size)
 
 
-def print_report(dct_, file_size, order):
-    """ prints to stdout summary of logs """
-    report = "File size: {}\n".format(file_size)
-    for key in order:
-        if dct_.get(key):
-            report += "{}: {}\n".format(key, dct_[key])
-    report = report[:-1]
-    print(report)
-
-
-if __name__ == "__main__":
-    main()
+except KeyboardInterrupt:
+    printsts(sts, size)
+    raise
