f362b26756324d73a5e4441597bf9f8a8153c545
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-23 16:29:03 +0000

    use logical xor to ensure higher bits are matched

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,17 +2,17 @@
 """ module's doc string """
 
 
-top_four_bits = 240
-blank_four_bytes = 8
+top_4_bits = 240
+blank_4_bytes = 8
 
-top_three_bits = 224
-blank_three_bytes = 16
+top_3_bits = 224
+blank_3_bytes = 16
 
-top_two_bits = 208
-blank_two_bytes = 32
+top_2_bits = 208
+blank_2_bytes = 32
 
-top_one_bit = 128
-blank_one_byte = 64
+top_1_bit = 128
+blank_1_byte = 64
 
 
 def validUTF8(data):
@@ -23,26 +23,26 @@ def validUTF8(data):
     if len(data) == 1:
         return data[0] < 128 and data[0] >= 0
 
-    while  index < len(data):
+    while index < len(data):
         byte = data[index]
-        if top_four_bits & byte >= top_four_bits and not blank_four_bytes & byte:
-            print("enterd")
-            if check_continuation_bytes(data, 3, index):
-                index += 4
-            else:
+        if byte > 127:
+            if top_4_bits ^ byte < top_4_bits and not blank_4_bytes & byte:
+                if check_continuation_bytes(data, 3, index):
+                    index += 4
+                else:
+                    return False
+            elif top_3_bits ^ byte < top_3_bits and not blank_3_bytes & byte:
+                if check_continuation_bytes(data, 2, index):
+                    index += 3
+                else:
+                    return False
+            elif top_2_bits ^ byte < top_2_bits and not blank_2_bytes & byte:
+                if check_continuation_bytes(data, 1, index):
+                    index += 2
+                else:
+                    return False
+            elif top_1_bit & byte:
                 return False
-        elif top_three_bits & byte >= top_three_bits and not blank_three_bytes & byte:
-            if check_continuation_bytes(data, 2, index):
-                index += 3
-            else:
-                return False
-        elif top_two_bits & byte >= top_two_bits and not blank_two_bytes & byte:
-            if check_continuation_bytes(data, 1, index):
-                index += 2
-            else:
-                return False
-        elif top_one_bit & byte:
-            return False
         else:
             index += 1
     return True
@@ -53,7 +53,7 @@ def check_continuation_bytes(data, no_of_continuation_bytes, current_index):
     index = current_index + 1
     end = index + no_of_continuation_bytes
     while index < end:
-        if data[index] & top_one_bit and not data[index] & blank_one_byte:
+        if data[index] & top_1_bit and not data[index] & blank_1_byte:
             index += 1
         else:
             return False
