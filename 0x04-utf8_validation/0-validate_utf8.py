b9692cc2e4e1ea7474f91d05659ff012967363d3
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-23 12:44:07 +0000

    add validUTF8 function

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" module's doc string """
+
+
+def validUTF8(data):
+    """  determines if a given data set represents a valid UTF-8 encoding """
+    for item in data:
+        if item > 127:
+            return False
+    return True
