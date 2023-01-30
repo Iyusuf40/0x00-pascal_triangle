cd30ba79142f0deb2a259c146bd42b9360b36493
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-30 17:13:50 +0000

    in the middle of nowhere

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -21,6 +21,18 @@ def main():
         print("N must be at least 4")
         sys.exit(1)
 
+    coexistable = []
+    all_indeces = (n * n) - 1
+    for num in range(n):
+        track_recursively(coexistable, num, n, all_indeces)
+
+    return coexistable
+
+
+def track_recursively(global_array, curr_no, length, max_index):
+    """ implements main logic """
+    pass
+
 
 def get_index(num, no_of_cols):
     """ returns the computed index of a flattened
@@ -62,6 +74,28 @@ def is_in_diagonal_path(curr_no, comp_no, n):
     return False
 
 
+def is_in_hor_or_ver_path(curr_no, comp_no, n):
+    """ checks if compared_no is in the horizontal or vertical
+    path of current_no based on get_index and get_row
+    """
+    if get_index(curr_no, n) == get_index(comp_no, n):
+        return True
+    if get_row(curr_no, n) == get_row(comp_no, n):
+        return True
+    return False
+
+
+def can_coexist(curr_no, comp_no, n):
+    """ checks if compared_no is not in a path accessible by
+    curr_no path of current_no based on get_index and get_row
+    """
+    if is_in_hor_or_ver_path(curr_no, comp_no, n):
+        return False
+    if is_in_diagonal_path(curr_no, comp_no, n):
+        return False
+    return True
+
+
 if __name__ == "__main__":
     main()
-    print(go_to_next_row(4, 4))
+    print(can_coexist(1, 15, 4))
