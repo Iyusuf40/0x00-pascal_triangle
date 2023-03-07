3ef91b9cb3df621059446a229289739e5bba58f4
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-03-07 13:24:29 +0000

    test improved cache

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """mod doc's"""
 
-cache = {'max': 0, 2: True, 3: True}
+cache = {'max': 3, 2: True, 3: True}
 
 
 def cache_primes(n):
     """ creates a cache of primes """
-    if n < cache['max']:
-        # print('no build', n, cache)
+    if n <= cache['max']:
         return
-    for x in range(2, n + 1):
+    for x in range(cache['max'], n + 1):
         if is_prime(x):
             cache[x] = True
 
@@ -18,8 +17,8 @@ def isWinner(x, nums):
     """ determines  who wins """
     Maria = 0
     Ben = 0
-    max_ = max(nums) if nums else 0
-    cache_primes(max_)
+    # max_ = max(nums) if nums else 0
+    # cache_primes(max_)
     for index in range(x):
         n = nums[index]
         winner = getWinner(n)
@@ -48,7 +47,7 @@ def getWinner(n):
     current_position = 0
     for num in lst:
         if num and (cache.get(num) or is_prime(num)):
-            inner_position = current_position
+            # inner_position = current_position
             # for to_remove in lst[current_position:]:
             #     if to_remove and to_remove % num == 0:
             #         lst[inner_position] = None
@@ -72,7 +71,7 @@ def is_prime(n):
     if n % 2 == 0:
         return False
     if cache.get(n):
-        # print('hit')
+        print('hit')
         return True
     if not cache.get(n) and n < cache['max']:
         # print(n, '\nmiss\n')
@@ -85,6 +84,7 @@ def is_prime(n):
             return False
         start += 2
     if n > cache['max']:
+        # print('max set:', n)
         cache['max'] = n
     cache[n] = True
     return True
